f1fa1a6083ed2778460ff1655a9e7f575753ed57
EC2 Default User <ec2-user@ip-172-31-92-225.ec2.internal>
2022-06-19 16:09:42 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+     @book = Book.all
+  end
+
+  def show
+     @book = Book.page(params[:page])
+     @post_comment = PostComment.new
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to Book.all
+  end
+
+# 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:book_title, :caption)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
+
   devise_for :users
 
-  resources :users, only: [:new, :index, :show]
+  root to: 'homes#top'
+  get "homes/about" => "homes#about" ,as: 'about'
+
+  resources :books, only: [:new, :index, :show, :edit, :create]
+  resources :users, only: [:index, :show, :edit]
+
 end
